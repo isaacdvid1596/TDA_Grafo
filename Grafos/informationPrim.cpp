8109a5bc5d7a8b094060a3072381590cb4514a3a
Julio Isaac David Ramos <isaacdvid15.96@gmail.com>
2021-11-25 22:38:05 -0600

    Delete informationPrim.cpp

--- Grafos/informationPrim.cpp
+++
@@ -1,9 +0,0 @@
-#include "informationPrim.h"
-
-InformationPrim::InformationPrim(string inicio,string fin, int peso)
-{
-    this->nombreFin=fin;
-    this->nombreInicio=inicio;
-    this->peso=peso;
-}
-
